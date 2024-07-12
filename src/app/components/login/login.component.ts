b0349280c9f333c2ab3b95340033d77602c8b1ce
Regige <144334703+Regige@users.noreply.github.com>
2024-07-12 17:30:34 +0200

    Added HttpClient

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { environment } from '../../../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -13,30 +15,40 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
 
+  constructor(private as:AuthService) {}
 
 
   async login() { // Logik um mit backend zu kommunizieren
 
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+    // const myHeaders = new Headers();
+    // myHeaders.append("Content-Type", "application/json");
 
 
-    const raw = JSON.stringify({
-      "username": this.username,
-      "password": this.password
-    });
+    // const raw = JSON.stringify({
+    //   "username": this.username,
+    //   "password": this.password
+    // });
 
-    const requestOptions:RequestInit = {
-      method: "POST",
-      headers: myHeaders,
-      body: raw,
-      redirect: "follow"
-    };
+    // const requestOptions:RequestInit = {
+    //   method: "POST",
+    //   headers: myHeaders,
+    //   body: raw,
+    //   redirect: "follow"
+    // };
+
+    // try {
+    //   let resp = await fetch(environment.baseURL + "/login/", requestOptions);
+    //   let json = await resp.json();
+    //   localStorage.setItem('token', json.token);
+    //   // TODO: Redirect
+    // } catch(e) {
+    //   // Show error message
+    //   console.error(e);
+    // }
 
     try {
-      let resp = await fetch(environment.baseURL + "/login/", requestOptions);
-      let json = await resp.json();
-      localStorage.setItem('token', json.token);
+      let resp = await this.as.loginWithUsernameAndPassword(this.username, this.password);
+      console.log(resp);
       // TODO: Redirect
     } catch(e) {
       // Show error message
@@ -44,6 +56,7 @@ export class LoginComponent {
     }
 
   }
+
 
 
 }
