28b920d1d2266795b365c799a889308040b15e17
Regige <144334703+Regige@users.noreply.github.com>
2024-07-17 17:31:58 +0200

    Added delete + update function

--- src/app/components/all-todos/all-todos.component.ts
+++ src/app/components/all-todos/all-todos.component.ts
@@ -35,7 +35,7 @@ export class AllTodosComponent {
     return lastValueFrom(this.http.get(url));
   }
 
-  
+
   async saveNewTodo() {
     try {
       const url = environment.baseURL + '/todos/';
@@ -55,4 +55,50 @@ export class AllTodosComponent {
       console.error(e);
     }
   }
+
+
+  async deleteTodo(index:number) {
+    const todoId = this.todos[index].id
+    try {
+      const url = environment.baseURL + '/todos/' + todoId;
+
+      let resp: any = await lastValueFrom(this.http.delete(url));
+      
+      console.log('So sieht die resp aus', resp);
+
+      if (resp.message === 'Todo Item successfully deleted') {
+        this.todos.splice(index, 1); 
+        console.log('Todo Item gelöscht');
+      }
+    } catch(e) {
+        alert('Delete fehlgeschlagen');
+        console.error(e);
+    }
+  }
+
+
+  async updateTodo(index:number, event: Event) {
+    const todoId = this.todos[index].id;
+    const inputElement = event.target as HTMLInputElement;
+    const checked = inputElement.checked;
+    try {
+      const url = environment.baseURL + '/todos/' + todoId + '/';
+      const body = {
+        "title": this.todos[index].title,
+        "checked": checked,
+        "author": this.todos[index].author,
+        "created_at": this.todos[index].created_at
+      }
+
+      let resp: any = await lastValueFrom(this.http.put(url, body));
+      console.log('So sieht die resp aus', resp);
+
+    } catch(e) {
+        alert('Update fehlgeschlagen');
+        console.error(e);
+    }
+  }
+
 }
+
+
